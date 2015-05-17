ed18a1f405704b5fb3b98600708d6968c4345719
eddyhub <eddyhub@users.noreply.github.com>
2015-05-17 22:34:02 +0200

    Added initial setup for publish-over-ssh plugin support.

--- manifests/plugins/publish_over_ssh.pp
+++ manifests/plugins/publish_over_ssh.pp
@@ -2,26 +2,38 @@
 #
 # parameter: see plugin.pp for a detailed description
 class jenkins::plugins::publish_over_ssh(
-  $version     = latest,
-  $ensure      = present,
-  $plugin_host = $jenkins::params::plugin_host,
-  $status      = enabled,
+  $encrypted_passphrase = undef,
+  $key = undef,
+  $disable_all_exec = true,
+  $version = latest,
+  $url     = undef,
+  $ensure  = present,
+  $status  = enabled,
   ) {
 
-  $config_file      = 'jenkins.plugins.publish_over_ssh.BapSshPublisherPlugin.xml'
+  validate_string($encrypted_passphrase)
+  validate_string($key)
+  validate_bool($disable_all_exec)
+
+  $init_dir = $jenkins::params::init_dir
+  $plugin_config_configs_order = $jenkins::params::plugin_config_configs_order
+
+  $config_file = 'jenkins.plugins.publish_over_ssh.BapSshPublisherPlugin.xml'
+
   jenkins::plugin {'publish-over-ssh':
-    version     => $version,
-    plugin_host => $plugin_host,
-    ensure      => $ensure,
-    status      => $status,
-  }->
-  file {"${jenkins::params::home_dir}/${config_file}":
+    version => $version,
+    url     => $url,
+    ensure  => $ensure,
+    status  => $status,
+  }
+
+  file {"${init_dir}/${plugin_config_configs_order}-plugin__publish-over-ssh__config.groovy":
     ensure => present,
-    source => "puppet:///modules/${module_name}/${config_file}",
+    content => template("jenkins/publish_over_ssh_plugin_config.groovy.erb"),
     owner   => $jenkins::params::username,
     group   => $jenkins::params::group,
-    mode    => '0644',
-    replace => false,
-    notify  => Service['jenkins']
+    mode    => '0600',
+    notify  => Service['jenkins'],
   }
+
 }
--- manifests/plugins/publish_over_ssh/config.pp
+++ manifests/plugins/publish_over_ssh/config.pp
@@ -11,10 +11,6 @@
 # limitations under the License.
 #
 # Type jenkins::plugins::publish_over_ssh::config
-#
-# Jenkins credentials_domain (via the CloudBees Credentials plugin, which is installed by default)
-# Generate UUID: ruby -e "require 'securerandom'; puts SecureRandom.uuid"
-#
 
 define jenkins::plugins::publish_over_ssh::config (
   $hostname = $title,
@@ -36,53 +32,65 @@ define jenkins::plugins::publish_over_ssh::config (
  #validate_string($password)
 
   Class['Jenkins::Plugins::Publish_over_ssh'] -> Jenkins::Plugins::Publish_over_ssh::Config["${title}"]
+
+  $init_dir = $jenkins::params::init_dir
+  $plugin_config_configs_order = $jenkins::params::plugin_config_configs_order
   $config_file_path = "${jenkins::params::home_dir}/${jenkins::plugins::publish_over_ssh::config_file}"
 
-  ### BEGIN: RESOURCE DEFAULT -- jenkins_augeas
-  # Set a default for the jenkins_augeas resource
-  Jenkins_Augeas {
-    incl => "${config_file_path}",
-    context => "/files/${config_file_path}/jenkins.plugins.publish__over__ssh.BapSshPublisherPlugin_-Descriptor/hostConfigurations/",
-    notify => Service['jenkins'],
-    require => Class['Jenkins::Plugins::Publish_over_ssh']
-  }
-  ### END: RESOURCE DEFAULT -- jenkins_augeas
+  # file {"${init_dir}/${plugin_config_configs_order}-plugin__publish-over-ssh_${title}__config.groovy":
+  #   ensure  => $ensure,
+  #   content => template("jenkins/publish_over_ssh_plugin_config.groovy.erb"),
+  #   owner   => $jenkins::params::username,
+  #   group   => $jenkins::params::group,
+  #   mode    => '0600',
+  #   notify  => Service['jenkins'],
+  # }
+
+  # ### BEGIN: RESOURCE DEFAULT -- jenkins_augeas
+  # # Set a default for the jenkins_augeas resource
+  # Jenkins_Augeas {
+  #   incl => "${config_file_path}",
+  #   context => "/files/${config_file_path}/jenkins.plugins.publish__over__ssh.BapSshPublisherPlugin_-Descriptor/hostConfigurations/",
+  #   notify => Service['jenkins'],
+  #   require => Class['Jenkins::Plugins::Publish_over_ssh']
+  # }
+  # ### END: RESOURCE DEFAULT -- jenkins_augeas
 
 
-  jenkins_augeas { "${config_file_path}--configuring publish_over_ssh plugin: ${title}":
-    changes => [
-                "set jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"]/name/#text \"${name}\"",
-                "defnode credentials_node jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"] \"\"",
-                "set \$credentials_node/hostname/#text \"${hostname}\"",
-                "set \$credentials_node/username/#text \"${username}\"",
-                "eset \$credentials_node/secretPassword/#text \"${password}\"",
-                "set \$credentials_node/remoteRootDir/#text \"${remote_root_dir}\"",
-                "set \$credentials_node/port/#text \"${port}\"",
-                "set \$credentials_node/timeout/#text \"${timeout}\"",
-                "set \$credentials_node/overrideKey/#text \"${override_key}\"",
-                "set \$credentials_node/disableExec/#text \"${disable_exec}\"",
-                ],
-  }
+  # jenkins_augeas { "${config_file_path}--configuring publish_over_ssh plugin: ${title}":
+  #   changes => [
+  #               "set jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"]/name/#text \"${name}\"",
+  #               "defnode credentials_node jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"] \"\"",
+  #               "set \$credentials_node/hostname/#text \"${hostname}\"",
+  #               "set \$credentials_node/username/#text \"${username}\"",
+  #               "eset \$credentials_node/secretPassword/#text \"${password}\"",
+  #               "set \$credentials_node/remoteRootDir/#text \"${remote_root_dir}\"",
+  #               "set \$credentials_node/port/#text \"${port}\"",
+  #               "set \$credentials_node/timeout/#text \"${timeout}\"",
+  #               "set \$credentials_node/overrideKey/#text \"${override_key}\"",
+  #               "set \$credentials_node/disableExec/#text \"${disable_exec}\"",
+  #               ],
+  # }
 
-  if $passphrase != undef {
-    validate_string($private_key_string)
-    jenkins_augeas { "${config_file_path}--configuring publish_over_ssh plugin: ${title} -- passphrase":
-      changes => [
-                  "defnode credentials_node jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"] \"\"",
-                  "eset \$credentials_node/keyInfo/secretPassphrase/#text \"${passphrase}\"",
-                  ],
-      require => Jenkins_augeas["${config_file_path}--configuring publish_over_ssh plugin: ${title}"]
-    }
-  }
+  # if $passphrase != undef {
+  #   validate_string($private_key_string)
+  #   jenkins_augeas { "${config_file_path}--configuring publish_over_ssh plugin: ${title} -- passphrase":
+  #     changes => [
+  #                 "defnode credentials_node jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"] \"\"",
+  #                 "eset \$credentials_node/keyInfo/secretPassphrase/#text \"${passphrase}\"",
+  #                 ],
+  #     require => Jenkins_augeas["${config_file_path}--configuring publish_over_ssh plugin: ${title}"]
+  #   }
+  # }
 
-  if $private_key_string != undef {
-    validate_string($private_key_string)
-    jenkins_augeas { "${config_file_path}--configuring publish_over_ssh plugin: ${title} -- private-key":
-      changes => [
-                  "defnode credentials_node jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"] \"\"",
-                  "set \$credentials_node/keyInfo/key/#text \"${private_key_string}\"",
-                  ],
-      require => Jenkins_augeas["${config_file_path}--configuring publish_over_ssh plugin: ${title}"]
-    }
-  }
+  # if $private_key_string != undef {
+  #   validate_string($private_key_string)
+  #   jenkins_augeas { "${config_file_path}--configuring publish_over_ssh plugin: ${title} -- private-key":
+  #     changes => [
+  #                 "defnode credentials_node jenkins.plugins.publish__over__ssh.BapSshHostConfiguration[name/#text = \"${title}\"] \"\"",
+  #                 "set \$credentials_node/keyInfo/key/#text \"${private_key_string}\"",
+  #                 ],
+  #     require => Jenkins_augeas["${config_file_path}--configuring publish_over_ssh plugin: ${title}"]
+  #   }
+  # }
 }
